d8c7677bde7105fa95c29866be1c6240a498a934
Unknown <bejongyang@gmail.com>
2017-08-12 18:31:46 -0500

    Multi Task
    Added Multi Task, key input is required to stop console now.

---
+++ Yennefer-bot/Model/YenneferSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yennefer_bot.Model
+{
+    public class YenneferSetting
+    {
+        public double MonopolyMoney { get; set; }
+    }
+}
--- Yennefer-bot/Program.cs
+++ Yennefer-bot/Program.cs
@@ -1,22 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Yennefer_bot
 {
-    static class Program
+    internal static class Program
     {
+        private static CancellationTokenSource TokenSource = new CancellationTokenSource();
+        private static CancellationToken Token = TokenSource.Token;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
+        public static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            MultiTask().Wait();
+        }
+
+        //Method to Run Forever Until Cancelled
+        private static void ExecuteStrategy()
+        {
+            while (!Token.IsCancellationRequested)
+            {
+                //Kill Key Value
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true);
+
+                    if (key.Key == ConsoleKey.X)
+                    {
+                        TokenSource.Cancel();
+                    }
+                }
+              
+            }
+        }
+
+        //Create New Task to run. 
+        static async Task MultiTask()
+        {
+            var testThread = new Task(ExecuteStrategy);
+            testThread.Start();
+            await testThread;
+         
         }
     }
 }
