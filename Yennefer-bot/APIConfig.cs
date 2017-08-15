044db78355eebd13870b550b60b77d7ce14f4d73
Unknown <bejongyang@gmail.com>
2017-08-14 21:03:38 -0500

    Push for Chad
    Meow

---
+++ Yennefer-bot/APIConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yennefer_bot
+{
+    public class APIConfig
+    {
+        public string API_KEY { get; set; }
+        public string ACCOUNT_ID { get; set; }
+    }
+}
--- Yennefer-bot/Program.cs
+++ Yennefer-bot/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using RestApi;
+using RestApi.DataTypes;
 
 namespace Yennefer_bot
 {
@@ -11,17 +15,32 @@ namespace Yennefer_bot
     {
         private static CancellationTokenSource TokenSource = new CancellationTokenSource();
         private static CancellationToken Token = TokenSource.Token;
+        private static APIConfig Config;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         public static void Main(string[] args)
         {
+            var readConfig = File.ReadAllText("Config.json");
+            Config = JsonConvert.DeserializeObject<APIConfig>(readConfig);
+            Rest.ApiKey = Config.API_KEY;
+
             MultiTask().Wait();
         }
 
         //Method to Run Forever Until Cancelled
         private static void ExecuteStrategy()
         {
+            Console.WriteLine("#####################\n Yennefer Bot Online \n#####################");
+
+            var account = Rest.GetAccountDetails(Config.ACCOUNT_ID);
+            var candles = Rest.GetCandles("EUR_USD", Config.ACCOUNT_ID);
+                      
+            Console.WriteLine("\n");
+            Console.WriteLine(@"Monopoly Money: " + account.balance);
+            Console.WriteLine(@"Candles: " + candles );
+            
             while (!Token.IsCancellationRequested)
             {
                 //Kill Key Value
@@ -33,18 +52,16 @@ namespace Yennefer_bot
                     {
                         TokenSource.Cancel();
                     }
-                }
-              
+                }     
             }
         }
-
+        
         //Create New Task to run. 
         static async Task MultiTask()
         {
             var testThread = new Task(ExecuteStrategy);
             testThread.Start();
-            await testThread;
-         
+            await testThread;    
         }
     }
 }
