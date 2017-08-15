88da0fa2c9b4313359b2264f980f8a7c7513ac42
Chad Smith <chadsmith4@my.unt.edu>
2017-08-14 22:53:33 -0500

    .net Core Refactor
    Refacotred the bot the REST API to be compatible with .net core.

---
+++ YenniferBotCore/Models/ApiSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YenniferBotCore.Models
+{
+    public class ApiSettings
+    {
+        public string ApiKey { get; set; }
+        public string AccountId { get; set; }
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ YenniferBotCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YenniferBotCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.StartBot().Wait();
+        }
+    }
+}
